ea6079f3ee617296596f590afdd17c16af6bf853
cou2jpn <anemone.reef@gmail.com>
2015-08-26 15:34:43 +0900

    Hide the link on main menu to anonymous.

--- init.rb
+++ init.rb
@@ -6,5 +6,5 @@ Redmine::Plugin.register :redmine_estimate_timelog do
   description 'This is a plugin for Redmine'
   version '0.6.0'
   requires_redmine :version_or_higher => '3.0.0'
-  menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption => :et_label_menu
+  menu :top_menu, :redmine_estimate_timelog, {:controller => 'estimate_timelog', :action => 'report'}, :caption => :et_label_menu, :if => Proc.new { User.current.logged? }
 end
